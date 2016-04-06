fd5898773e8007bbe560d0ae568c31444d417b9a
Xavier Corbillon <xavier.corbillon@telecom-bretagne.eu>
2016-04-06 14:26:17 +0200

    Remove need for std::underlying_type : could be better

--- transformation/src/common.hpp
+++ transformation/src/common.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <boost/range/iterator_range.hpp>
 #include <stdexcept>
 
 
@@ -128,8 +129,50 @@ inline bool AlmostEqual(double a, double b)
    return inIntervalStrict(a, b-0.0005f, b+0.0005f);
 }
 
+template<class enum_type>
+class enum_iterator {
+private:
+    enum_type value;
+    //typedef typename std::underlying_type<enum_type>::type under;
+    typedef int under;
+public:   
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+    typedef enum_type value_type;
+    typedef enum_type reference;
+    typedef enum_type* pointer;
+    typedef std::random_access_iterator_tag iterator_category;
+    
+    constexpr enum_iterator() :value() {}
+    constexpr enum_iterator(const enum_iterator& rhs) noexcept(true) :value(rhs.value) {}
+    constexpr explicit enum_iterator(enum_type value_) noexcept(true) :value(value_) {}
+    ~enum_iterator() noexcept(true) {}
+    enum_iterator& operator=(const enum_iterator& rhs) noexcept(true) {value=rhs.value; return *this;}
+    enum_iterator& operator++() noexcept(true) {value = (enum_type)(under(value) + 1); return *this;}
+    enum_iterator operator++(int) noexcept(true) {enum_iterator r(*this); ++*this; return r;}
+    enum_iterator& operator+=(size_type o) noexcept(true) {value = (enum_type)(under(value) + o); return *this;}
+    friend constexpr enum_iterator operator+(const enum_iterator& it, size_type o) noexcept(true) {return enum_iterator((enum_type)(under(it)+o));}
+    friend constexpr enum_iterator operator+(size_type o, const enum_iterator& it) noexcept(true) {return enum_iterator((enum_type)(under(it)+o));}
+    enum_iterator& operator--() noexcept(true) {value = (enum_type)(under(value) - 1); return *this;}
+    enum_iterator operator--(int) noexcept(true) {enum_iterator r(*this); --*this; return r;}
+    enum_iterator& operator-=(size_type o) noexcept(true) {value = (enum_type)(under(value) + o); return *this;}
+    friend constexpr enum_iterator operator-(const enum_iterator& it, size_type o) noexcept(true) {return enum_iterator((enum_type)(under(it)-o));}
+    friend constexpr difference_type operator-(enum_iterator lhs, enum_iterator rhs) noexcept(true) {return under(lhs.value)-under(rhs.value);}
+    constexpr reference operator*() const noexcept(true) {return value;}
+    constexpr reference operator[](size_type o) const noexcept(true) {return (enum_type)(under(value) + o);}
+    constexpr const enum_type* operator->() const noexcept(true) {return &value;}
+    constexpr friend bool operator==(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value==rhs.value;}
+    constexpr friend bool operator!=(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value!=rhs.value;} 
+    constexpr friend bool operator<(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value<rhs.value;}
+    constexpr friend bool operator>(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value>rhs.value;}
+    constexpr friend bool operator<=(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value<=rhs.value;}
+    constexpr friend bool operator>=(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value>=rhs.value;}
+    friend void swap(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {std::swap(lhs.value, rhs.value);}
+};
+template<class enum_type> constexpr boost::iterator_range<enum_iterator<enum_type>> get_range() noexcept(true) 
+{return boost::make_iterator_range(enum_iterator<enum_type>(enum_type::First), enum_iterator<enum_type>(enum_type::Last));}
+
+
+
+
 }    
-     
-     
- 
-     
--- transformation/src/layout.hpp
+++ transformation/src/layout.hpp
@@ -2,7 +2,6 @@
 
 #include <opencv2/opencv.hpp>
 #include <cmath>
-#include <boost/range/iterator_range.hpp>
 #include <memory>
 #include "picture.hpp"
 
@@ -46,49 +45,6 @@ class Layout
         unsigned int m_outHeight;
     private:
 };
-
-template<class enum_type>
-class enum_iterator {
-private:
-    enum_type value;
-    typedef typename std::underlying_type<enum_type>::type under;
-public:   
-    typedef std::size_t size_type;
-    typedef std::ptrdiff_t difference_type;
-    typedef enum_type value_type;
-    typedef enum_type reference;
-    typedef enum_type* pointer;
-    typedef std::random_access_iterator_tag iterator_category;
-    
-    constexpr enum_iterator() :value() {}
-    constexpr enum_iterator(const enum_iterator& rhs) noexcept(true) :value(rhs.value) {}
-    constexpr explicit enum_iterator(enum_type value_) noexcept(true) :value(value_) {}
-    ~enum_iterator() noexcept(true) {}
-    enum_iterator& operator=(const enum_iterator& rhs) noexcept(true) {value=rhs.valud; return *this;}
-    enum_iterator& operator++() noexcept(true) {value = (enum_type)(under(value) + 1); return *this;}
-    enum_iterator operator++(int) noexcept(true) {enum_iterator r(*this); ++*this; return r;}
-    enum_iterator& operator+=(size_type o) noexcept(true) {value = (enum_type)(under(value) + o); return *this;}
-    friend constexpr enum_iterator operator+(const enum_iterator& it, size_type o) noexcept(true) {return enum_iterator((enum_type)(under(it)+o));}
-    friend constexpr enum_iterator operator+(size_type o, const enum_iterator& it) noexcept(true) {return enum_iterator((enum_type)(under(it)+o));}
-    enum_iterator& operator--() noexcept(true) {value = (enum_type)(under(value) - 1); return *this;}
-    enum_iterator operator--(int) noexcept(true) {enum_iterator r(*this); --*this; return r;}
-    enum_iterator& operator-=(size_type o) noexcept(true) {value = (enum_type)(under(value) + o); return *this;}
-    friend constexpr enum_iterator operator-(const enum_iterator& it, size_type o) noexcept(true) {return enum_iterator((enum_type)(under(it)-o));}
-    friend constexpr difference_type operator-(enum_iterator lhs, enum_iterator rhs) noexcept(true) {return under(lhs.value)-under(rhs.value);}
-    constexpr reference operator*() const noexcept(true) {return value;}
-    constexpr reference operator[](size_type o) const noexcept(true) {return (enum_type)(under(value) + o);}
-    constexpr const enum_type* operator->() const noexcept(true) {return &value;}
-    constexpr friend bool operator==(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value==rhs.value;}
-    constexpr friend bool operator!=(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value!=rhs.value;} 
-    constexpr friend bool operator<(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value<rhs.value;}
-    constexpr friend bool operator>(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value>rhs.value;}
-    constexpr friend bool operator<=(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value<=rhs.value;}
-    constexpr friend bool operator>=(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {return lhs.value>=rhs.value;}
-    friend void swap(const enum_iterator& lhs, const enum_iterator& rhs) noexcept(true) {std::swap(lhs.value, rhs.value);}
-};
-template<class enum_type> constexpr boost::iterator_range<enum_iterator<enum_type>> get_range() noexcept(true) 
-{return boost::make_iterator_range(enum_iterator<enum_type>(enum_type::First), enum_iterator<enum_type>(enum_type::Last));}
-
 
 
 }
